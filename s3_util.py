90dfb933f66a2d434f379567b823411d7364dedf
unknown <hchalla2@asu.edu>
2023-02-26 12:25:01 -0700

    S3 added

--- s3_util.py
+++ s3_util.py
@@ -2,13 +2,5 @@ import boto3
 
 s3 = boto3.resource("s3", aws_access_key_id='AKIA52M3AL3AZ3O222CR', aws_secret_access_key='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')
 
-def store_file(bucket_name, file_content, object_name):
-        bucket = s3.Bucket(bucket_name)
-        response = bucket.put_object(
-        ACL="private",
-        Body=file_content,
-        ServerSideEncryption="AES256",
-        Key=object_name,
-        Metadata={"env": "dev", "owner": "binary guy"},
-        )
-
+def store_file(bucket_name, file_path, object_name):
+        s3.meta.client.upload_file(file_path, bucket_name, object_name)
