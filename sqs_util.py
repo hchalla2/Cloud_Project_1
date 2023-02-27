60a0f010f764c720b85a717e261c5fa3be69516d
unknown <hchalla2@asu.edu>
2023-02-26 19:36:45 -0700

    Old files

--- sqs_util.py
+++ sqs_util.py
@@ -4,15 +4,21 @@ from config import *;
 sqs = boto3.client("sqs", aws_access_key_id=get_access_key(), aws_secret_access_key=get_secret_key(), region_name='us-east-1');
 
 def send_message(queue_url, body):
-    sqs.send_message(QueueUrl=queue_url, DelaySeconds=10, MessageBody=body)
-    return 0;
+    try:
+        sqs.send_message(QueueUrl=queue_url, DelaySeconds=10, MessageBody=body)
+    except:
+        print("Sending message to queue has failed");
 
 def delete_message(queue_url, recipient_handle):
-    sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=recipient_handle);
-    return 0;
+    try:
+        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=recipient_handle);
+    except:
+        print("Deleting message in the queue has failed")
 
 def receive_message(queue_url):
-    response = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=1, WaitTimeSeconds=10)
-    return response;
-
+    try:
+        response = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=1, WaitTimeSeconds=10)
+        return response;
+    except:
+        print("Receive operation from the queue didn't work");
 
